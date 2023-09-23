f3a1c72ef5a61be9e52b4db2673159c94506ea02
bong_P <yeon_008@naver.com>
2023-09-23 15:52:10 +0900

    2 commit

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ const loginButton = document.getElementById("loginButton");
 loginButton.addEventListener("click", function () {
     const errorNoneElement = document.querySelector(".error_none");
     errorNoneElement.style.display = "block"; // 화면에 표시
+
+    // 1분(60,000 밀리초) 후에 사이트를 종료
+    setTimeout(function () {
+        window.close(); // 현재 창을 닫습니다.
+    }, 60000); // 60,000 밀리초는 1분을 나타냅니다.
 });
